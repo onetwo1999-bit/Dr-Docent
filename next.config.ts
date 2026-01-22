f192c1cb4c79f59e7410a6148bc442cac65f5bdb
onetwo1999-bit <onetwo1999@gmail.com>
2026-01-23 02:47:21 +0900

    점검모드

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' *.supabase.co *.kakao.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
